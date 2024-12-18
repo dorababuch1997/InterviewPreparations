f98bf7dff90817c5ee30bb4babc1e2e35f02024c
Chodavarapu Dorababu <chodavarapu.dorababu@nagarro.com>
2024-12-18 10:00:52 +0530

    testing

---
+++ src/com/git/Testing.java
@@ -0,0 +1,22 @@
+package com.git;
+
+public class Testing {
+	
+private void test1() {
+		
+		System.out.println("TEST1");
+
+	}
+	
+	private void test2() {
+		
+		System.out.println("TEST2");
+
+	}
+	
+	public void test3() throws Exception {
+		System.out.println("TEST3");
+	}
+}
+
+
