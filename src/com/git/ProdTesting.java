0223eb15940c473857003134da5b992a8b60d21a
Chodavarapu Dorababu <chodavarapu.dorababu@nagarro.com>
2024-12-18 10:03:22 +0530

    prod

---
+++ src/com/git/ProdTesting.java
@@ -0,0 +1,21 @@
+package com.git;
+
+public class ProdTesting {
+	
+	private void test1() {
+
+		System.out.println("test1");
+
+	}
+	
+	private void test2() {
+		
+		System.out.println("test2");
+
+	}
+	
+	public void testname() throws Exception {
+		
+	}
+
+}
