34c9640ec924395b672bf7f696c9e0da28b85dca
Chodavarapu Dorababu <chodavarapu.dorababu@nagarro.com>
2024-12-18 10:11:11 +0530

    testing

--- src/com/git/DevTesting.java
+++ src/com/git/DevTesting.java
@@ -1,5 +1,19 @@
 package com.git;
 
 public class DevTesting {
+	
+	
+
+	private void test1() {
+		
+		System.out.println("TEST1");
+
+	}
+	
+	private void test2() {
+		
+		System.out.println("TES2");
+
+	}
 
 }
